4c78dc3e22cad20a623a2090689c39320ab65759
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-18 13:27:27 -0800

    Add project files.

---
+++ COMP003A.LectureActivity6-2/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+// Author: Isaac Martinez
+// Course: COMP-003A
+// Faculty: Jonathan Cruz
+// Purpose: OOP Polymorphism
+*/
+namespace COMP003A.LectureActivity6_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
