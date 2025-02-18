309cefc302736c831f3fee10ff0fbf9170524ed9
Isaac Martinez <isaacmartinez2429@gmail.com>
2025-02-18 13:52:58 -0800

    check point

---
+++ COMP003A.LectureActivity6-2/Circle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_2
+{
+    /// <summary>
+    /// Represents a circle with a specific are calculation.
+    /// </summary>
+    class Circle : Shape
+    {
+        // Auto-implemented property for the raduis of the circle
+        public double Raduis {  get; set; }
+
+        /// <summary>
+        /// Constructor for the Circle Class
+        /// </summary>
+        /// <param name="raduis"></param>
+        public Circle(double radius)
+        {
+            Raduis = radius;
+        }
+
+        /// <summary>
+        /// Display the area of a circle.
+        /// </summary>
+        public override void DisplayArea()
+        {
+            Console.WriteLine($"Circle Area: {Math.PI * Raduis * Raduis}");
+        }
+    }
+}
---
+++ COMP003A.LectureActivity6-2/ShapeCalculator.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.LectureActivity6_2
+{
+    /// <summary>
+    /// Demonstrate method overloading for different shape calculations
+    /// </summary>
+    class ShapeCalculator
+    {
+        /// <summary>
+        /// Caclculates the area of a circle.
+        /// </summary>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public double CalculateArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+
+        /// <summary>
+        /// Calculate the area of a rectangle
+        /// </summary>
+        /// <param name="length"></param>
+        /// <param name="width"></param>
+        /// <returns></returns>
+        public double CalculateArea(double length, double width)
+        {
+            return length * width;
+        }
+
+        /// <summary>
+        /// Calculate the area of a triangle
+        /// </summary>
+        /// <param name="baseLength"></param>
+        /// <param name="height"></param>
+        /// <param name="isTriangle"></param>
+        /// <returns></returns>
+        public double CalculateArea(double baseLength, double height, bool isTriangle)
+        {
+            return 0.5 * baseLength * height;
+        }
+    }
+}
